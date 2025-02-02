d270e569402e2c0299835c4789a9bd083b41e9e1
Sushant <sushantnadavade006@gmail.com>
2025-02-02 22:58:15 +0530

    theme added

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+   
+
+   static const primary = Color(0xff42c83C);
+   static const lightbackgroundcolor = Color(0xFFFFFFFF);
+   static const darkbackgroundcolor = Color(0xFF000000);
+
+   static const grey = Color(0xffBEBEBE);
+   static const dartGrey = Color(0xff343434);
+
+   
+}
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+   
+
+
+   // theme for the green button ....
+   static final lighttheme = ThemeData(
+    primaryColor:  AppColors.primary,
+    scaffoldBackgroundColor : AppColors.lightbackgroundcolor, 
+    brightness: Brightness.light,
+    elevatedButtonTheme : ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              )
+        )
+    ),
+
+
+   );
+
+   
+
+
+   //dark theme for the button .....
+   static final darktheme = ThemeData(
+    primaryColor:  AppColors.primary,
+    scaffoldBackgroundColor : AppColors.darkbackgroundcolor, 
+    brightness: Brightness.light,
+    elevatedButtonTheme : ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primary,
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              )
+        )
+    ),
+
+
+   );
+
+
+
+}
